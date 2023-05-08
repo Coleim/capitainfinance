11a213cf2d31138a778c054aeb38f8e86a3c8a89
Clément Oliva <clement.oliva@gmail.com>
2023-05-08 10:20:06 +0200

    to fixed amount in list

--- app/components/DailyTransactionItem.tsx
+++ app/components/DailyTransactionItem.tsx
@@ -20,7 +20,7 @@ export function DailyTransactionItem(props) {
                     <Text style={ { color: "#525174", fontSize: 15 } } >{item.label}</Text>
                     <Text style={ { fontSize: 10 } } >{ date.AsString(new Date(item.date)) } </Text>
                 </View>
-                <Text style={ [{ marginLeft: "auto", fontWeight: "bold" }, item.amount > 0 ? {color: "#06d6a0"}: {color: "#ef476f"}] } >{item.amount} €</Text>
+                <Text style={ [{ marginLeft: "auto", fontWeight: "bold" }, item.amount > 0 ? {color: "#06d6a0"}: {color: "#ef476f"}] } >{item.amount.toFixed(2)} €</Text>
             </View>
         </Pressable>
       </View>
--- app/components/RecurringItem.tsx
+++ app/components/RecurringItem.tsx
@@ -20,7 +20,7 @@ export function RecurringItem(props) {
                     <Text style={ { color: "#525174", fontSize: 15 } } >{item.label}</Text>
                     {item.date && <Text style={ { fontSize: 10 } } >du { date.AsString(item.date) } au { date.AsString(item.endDate) } </Text>}
                 </View>
-                <Text style={ [{ marginLeft: "auto", fontWeight: "bold" }, item.amount > 0 ? {color: "#06d6a0"}: {color: "#ef476f"}] } >{item.amount} €</Text>
+                <Text style={ [{ marginLeft: "auto", fontWeight: "bold" }, item.amount > 0 ? {color: "#06d6a0"}: {color: "#ef476f"}] } >{item.amount.toFixed(2)} €</Text>
             </View>
         </Pressable>
       </View>
