5d28e964f5ff9d4e1973244fe9c8a8ace9f69c82
Clément Oliva <clement.oliva@gmail.com>
2023-05-19 13:33:48 +0200

    restyle menu bar

--- app/components/MenuBar.tsx
+++ app/components/MenuBar.tsx
@@ -28,46 +28,76 @@ export function MenuBar(props) {
 
 
   return (
-  
-    <View style={{ height: 70 }} >
-      <View style={{ flexDirection: "row", justifyContent: "flex-start", alignContent: "flex-start", alignSelf: 'center', backgroundColor: '#303030', 
-                     width: 135, height: 50, 
-                     borderBottomEndRadius: 30, borderBottomStartRadius: 30, left: 0, bottom: 0, zIndex: 2 }}>
-        <Ionicons name="md-add-circle" size={70} color="#1ee9a4" style={{zIndex: 3, bottom: 20, height: 70 }} onPress={ () => addIncome() } />
-        <Ionicons name="md-remove-circle" size={70} color="#e91e63" style={{zIndex: 3, bottom: 20, height: 70}} onPress={ () => addExpense() } />
-      </View>
-      <View style={{ position: 'absolute', backgroundColor: '#303030', bottom: 0, zIndex: 1, width: '100%', height: 60, 
-        flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10, paddingVertical: 10 }}>
-        
-        { isRecurring && <View style={{flex: 1, marginRight: "auto"}}>
+
+    <View style={{ flexDirection: "row", margin: 5, alignItems: "center" }}>
+      <View style={{ flex: 1 }}>
+        { isRecurring ?
           <Ionicons name="md-arrow-back" size={32} color="#fff" onPress={ () => goHome() } />
-        </View> }
-
-        { !isRecurring &&
-          <Pressable  style={ { flexDirection: "column", justifyContent: "center", alignItems: "center", marginRight: "auto" }} onPress={ () => openConfiguration() }>
-            <Ionicons name="md-settings" size={15} color="#fff" style={{zIndex: 4, height: 15, marginBottom: 5 }} />
-            <Text style={ { color: "#fff", marginRight: 5, fontSize: 10, alignSelf: "baseline"} }>Configuration</Text>
+          :
+          <Pressable style={{ flexDirection: "column", justifyContent: "center", alignItems: "center", marginRight: "auto" }} onPress={() => openConfiguration()}>
+            <Ionicons name="md-settings" size={15} color="#fff" style={{ zIndex: 4, height: 15, marginBottom: 5 }} />
+            <Text style={{ color: "#fff", marginRight: 5, fontSize: 10, alignSelf: "baseline" }}>Configuration</Text>
           </Pressable >
         }
-
-        {/* { !isRecurring && <Ionicons name="md-settings" size={15} color="#fff" /> } */}
-        {/* { isRecurring && <Ionicons name="md-arrow-back" size={32} color="#fff" onPress={ () => goHome() } /> } */}
-        
-
-        { !isRecurring &&
-          <Pressable style={ { marginLeft: "auto", flexDirection: "column", justifyContent: "center", alignItems: "center" }} onPress={ () => openRecurringConfigurationPage() }>
-            <Ionicons name="md-sync-circle-outline" size={15} color="#fff" style={{zIndex: 3, height: 15, marginBottom: 5 }} />
-            <Text style={ { color: "#fff", marginRight: 5, fontSize: 10, alignSelf: "baseline"} }>Transactions</Text>
-            <Text style={ { color: "#fff", marginRight: 5, fontSize: 10, alignSelf: "baseline"} }>récurrentes</Text>
-          </Pressable >
-        }
-
-        
-        {/* <View style={{ flexDirection: 'row' }}>
-          
-          { !isRecurring && <Ionicons name="md-calendar" size={32} color="#fff" style={{ marginLeft: 20 }} onPress={ () => openRecurringConfigurationPage() }/> }
-        </View> */}
       </View>
+
+      <View style={{ flex: 1, flexDirection: "row", marginLeft: "auto", marginBottom: -20 }}>
+        <Ionicons name="md-add-circle" size={70} style={{zIndex: 3, bottom: 25 }} color="#1ee9a4" onPress={ () => addIncome() } />
+        <Ionicons name="md-remove-circle" size={70} color="#e91e63" style={{zIndex: 3, bottom: 25 }} onPress={ () => addExpense() } />
+      </View>
+
+      <View style={{ flex: 1 }}>
+        { !isRecurring &&
+
+          <Pressable style={ { marginLeft: "auto", flexDirection: "column", justifyContent: "center", alignItems: "center", alignContent: "center" }} onPress={ () => openRecurringConfigurationPage() }>
+              <Ionicons name="md-sync-circle-outline" size={15} color="#fff" style={{zIndex: 3, height: 15, marginBottom: 5 }} />
+              <Text style={ { color: "#fff", marginRight: 5, fontSize: 10} }>Transactions</Text>
+              <Text style={ { color: "#fff", marginRight: 5, fontSize: 10} }>récurrentes</Text>
+            </Pressable >
+        }
+      </View>
+
     </View>
+  
+    // <View style={{ height: 70 }} >
+    //   <View style={{ flexDirection: "row", justifyContent: "flex-start", alignContent: "flex-start", alignSelf: 'center', backgroundColor: '#303030', 
+    //                  width: 135, height: 50, 
+    //                  borderBottomEndRadius: 30, borderBottomStartRadius: 30, left: 0, bottom: 0, zIndex: 2 }}>
+    //     <Ionicons name="md-add-circle" size={70} color="#1ee9a4" style={{zIndex: 3, bottom: 20, height: 70 }} onPress={ () => addIncome() } />
+    //     <Ionicons name="md-remove-circle" size={70} color="#e91e63" style={{zIndex: 3, bottom: 20, height: 70}} onPress={ () => addExpense() } />
+    //   </View>
+    //   <View style={{ position: 'absolute', backgroundColor: '#303030', bottom: 0, zIndex: 1, width: '100%', height: 60, 
+    //     flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10, paddingVertical: 10 }}>
+        
+    //     { isRecurring && <View style={{flex: 1, marginRight: "auto"}}>
+    //       <Ionicons name="md-arrow-back" size={32} color="#fff" onPress={ () => goHome() } />
+    //     </View> }
+
+    //     { !isRecurring &&
+    //       <Pressable  style={ { flexDirection: "column", justifyContent: "center", alignItems: "center", marginRight: "auto" }} onPress={ () => openConfiguration() }>
+    //         <Ionicons name="md-settings" size={15} color="#fff" style={{zIndex: 4, height: 15, marginBottom: 5 }} />
+    //         <Text style={ { color: "#fff", marginRight: 5, fontSize: 10, alignSelf: "baseline"} }>Configuration</Text>
+    //       </Pressable >
+    //     }
+
+    //     {/* { !isRecurring && <Ionicons name="md-settings" size={15} color="#fff" /> } */}
+    //     {/* { isRecurring && <Ionicons name="md-arrow-back" size={32} color="#fff" onPress={ () => goHome() } /> } */}
+        
+
+    //     { !isRecurring &&
+    //       <Pressable style={ { marginLeft: "auto", flexDirection: "column", justifyContent: "center", alignItems: "center" }} onPress={ () => openRecurringConfigurationPage() }>
+    //         <Ionicons name="md-sync-circle-outline" size={15} color="#fff" style={{zIndex: 3, height: 15, marginBottom: 5 }} />
+    //         <Text style={ { color: "#fff", marginRight: 5, fontSize: 10, alignSelf: "baseline"} }>Transactions</Text>
+    //         <Text style={ { color: "#fff", marginRight: 5, fontSize: 10, alignSelf: "baseline"} }>récurrentes</Text>
+    //       </Pressable >
+    //     }
+
+        
+    //     {/* <View style={{ flexDirection: 'row' }}>
+          
+    //       { !isRecurring && <Ionicons name="md-calendar" size={32} color="#fff" style={{ marginLeft: 20 }} onPress={ () => openRecurringConfigurationPage() }/> }
+    //     </View> */}
+    //   </View>
+    // </View>
   );
 }
