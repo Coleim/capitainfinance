010710c7cafeaef711248e6f5efe07aebb839bcb
Clément Oliva <clement.oliva@gmail.com>
2023-10-20 23:23:20 +0200

    use victory-native

--- app/pages/StatisticsPage.tsx
+++ app/pages/StatisticsPage.tsx
@@ -5,35 +5,57 @@ import { useSelector } from "react-redux";
 import { MenuBar } from "../components/MenuBar";
 import { useNavigation } from "@react-navigation/native";
 import { date } from "../services/DateAsString";
-import { BarChart } from "react-native-chart-kit";
 import { useEffect, useState } from "react";
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import { VictoryAxis, VictoryBar, VictoryChart, VictoryGroup, VictoryLabel, VictoryStack, VictoryTheme } from "victory-native";
+
+const NegativeAwareTickLabel = props => {
+  const {
+    datum, // Bar's specific data object
+    y, // Calculated y data value IN SVG SPACE (from top-right corner)
+    dy, // Distance from data's y value to label's y value
+    scale, // Function that converts from data-space to svg-space
+    ...rest // Other props passed to label from Bar
+  } = props;
+
+  return (
+    <VictoryLabel
+      datum={datum} // Shove `datum` back into label. We destructured it from `props` so we'd have it available for a future step
+      y={scale.y(0)} // Set y to the svg-space location of the axis
+      dy={20 * (datum.y === 0 ? 1 : Math.sign(datum.y))} // Change direction of offset based on the datum value
+      {...rest} // Shove the rest of the props into the label
+    />
+  );
+};
+
 
 export const StatisticsPage = () => {
-   
+
   const navigation = useNavigation();
   const today = new Date();
-  let lastMonth = new Date();
-  lastMonth.setDate(1);
-  lastMonth.setMonth(lastMonth.getMonth()-4);
   // const savings = useSelector( state => Object.values(state.savings).filter( saving => new Date(saving.savingDate).getFullYear() === today.getFullYear() ) );
-  const savings = useSelector( state => state.savings );
-  const allTransactionPerMonth = useSelector( state => state.transactions.allTransactionPerMonth );
+  const savings = useSelector(state => state.savings);
+  const allTransactionPerMonth = useSelector(state => state.transactions.allTransactionPerMonth);
 
   const [totalSaving, setTotalSaving] = useState(0);
   const [savingData, setSavingData] = useState([]);
-  const [categoriesLabel, setCategoriesLabel] = useState([]);
   const [categoriesData, setCategoriesData] = useState([]);
-  
+  let lastMonth = new Date();
+  lastMonth.setDate(1);
+  lastMonth.setMonth(lastMonth.getMonth() - 1);
+  const [currentDetailsMonth, setCurrentDetailsMonth] = useState(lastMonth);
+
   function getSavings() {
     let data = [];
     let chartLabelsIdx = 0;
     let savingIdx = 0;
-    
-    let filteredSavings = Object.values(savings).filter( saving => new Date(saving.savingDate).getFullYear() === today.getFullYear() )
-    while(chartLabelsIdx < 12) {
+
+    let filteredSavings = Object.values(savings).filter(saving => new Date(saving.savingDate).getFullYear() === today.getFullYear())
+    while (chartLabelsIdx < 12) {
       const saving = filteredSavings[savingIdx]
-      if(saving && chartLabelsIdx === Number(date.GetMonthFromKey(saving.key))) {
-        data.push(saving.amount.toFixed())
+      if (saving && chartLabelsIdx === Number(date.GetMonthFromKey(saving.key))) {
+        data.push(Number(saving.amount.toFixed()))
         ++savingIdx
       } else {
         data.push(0)
@@ -44,162 +66,150 @@ export const StatisticsPage = () => {
   }
 
   function getCurrentMonthTransaction() {
-    const currentMonthKey = date.GetMonthKey(lastMonth);
-    console.log(currentMonthKey)
+    const currentMonthKey = date.GetMonthKey(currentDetailsMonth);
     const currMonthTransations = allTransactionPerMonth[currentMonthKey];
-    currMonthTransations.dailyTransactions;
-    currMonthTransations.recurringTransactions;
-    console.log(currMonthTransations.dailyTransactions)
-    console.log(currMonthTransations.recurringTransactions)
     let transMap = new Map();
-    currMonthTransations.dailyTransactions.forEach( transaction => {
-      if(transMap.has(transaction.category)) {
+    currMonthTransations?.dailyTransactions.forEach(transaction => {
+      if (transMap.has(transaction.category)) {
         let newAmount = Number(transMap.get(transaction.category)) + Number(transaction.amount);
         transMap.set(transaction.category, newAmount)
       } else {
         transMap.set(transaction.category, Number(transaction.amount))
       }
     })
-
     return transMap;
   }
 
   useEffect(() => {
     const savingsData = getSavings();
-    const transMap = getCurrentMonthTransaction();
-    let labels = []
-    let data = []
-    var mapAsc = new Map([...transMap.entries()].sort((a,b) => {
-      if(a>b) return -1;
-      if(a<b) return 1;
-      return 0;
-    }));
-    mapAsc.forEach( (value, key) => {
-      labels.push(key);
-      data.push(value*-1);
-    })
-    setCategoriesLabel(labels)
-    setCategoriesData(data)
-    setTotalSaving(savingsData.reduce((p, curr) => Number(curr) + p , 0))
-    setSavingData(savingsData);
+    const labels = ["Jan.", "Fév.", "Mar.", "Avr.", "Mai", "Juin", "Juil.", "Août", "Sep.", "Oct.", "Nov.", "Déc."]
+    const output = labels.reduce((acc, curr, i) => {
+      acc[i] = { ...acc[i], x: curr, y: savingsData[i] }
+      return acc
+    }, [])
+    setTotalSaving(savingsData.reduce((p, curr) => Number(curr) + p, 0))
+    setSavingData(output);
   }, [savings]);
 
-  function renderAllTransactions() {
-    let content = [];
-    let data = [];
-    
-    for ( let k of Object.keys(allTransactionPerMonth) ) {
-      console.log("KKK: " , k)
+  useEffect(() => {
+    const transMap = getCurrentMonthTransaction();  
+    var mapAsc = new Map([...transMap.entries()].sort((a, b) => {
+      if (Number(a[1]) > Number(b[1])) return -1;
+      if (Number(a[1]) < Number(b[1])) return 1;
+      return 0;
+    }));
+    let data = []
+    mapAsc.forEach((value, key) => {
+      data.push({
+        x: key,
+        y: Number((value * -1).toFixed()) });
+    })
+    // setCategoriesLabel(labels)
+    setCategoriesData(data)
+  }, [currentDetailsMonth]);
 
-      content.push(<Text key={k+"SEP01"} style={{ color: "#fff"}} >---------------------------------------</Text> )
-      content.push(<Text key={k} style={{ color: "#fff"}} >{ date.GetKeyDisplay(k) } </Text> )
-      const mT = allTransactionPerMonth[k];
-      content.push(<Text key={k+"SEP001"} style={{ color: "#fff"}} > ---- DAILY TRANSACTIONS ---- </Text> )
-      let dTTotal = 0;
-      mT.dailyTransactions.forEach( t => {
-        dTTotal += t.amount;
-      })
-      content.push(<Text key={k+"TOTAL"} style={{ color: "#fff"}} >{ dTTotal } </Text> )
-
-      content.push(<Text key={k+"SEP002"} style={{ color: "#fff"}} > ---- RECURRING TRANSACTIONS ---- </Text> )
-      let rTTotal = 0;
-      mT.recurringTransactions?.forEach( t => {
-        rTTotal += t.amount;
-      })
-      content.push(<Text key={k+"TOTAL2"} style={{ color: "#fff"}} >{ rTTotal } </Text> )
-    }
-    
-    return content
+  function nextMonth(): void {
+    setCurrentDetailsMonth(prev => {
+      let m = new Date(prev)
+      m.setMonth(prev.getMonth() + 1);
+      return m;
+    })
   }
 
-  const chartConfig = {
-    backgroundColor: "#ffffff",
-    backgroundGradientFrom: "#fff",
-    backgroundGradientTo: "#fff",
-    color: (opacity = 1) => `rgba(100, 100, 255, 1)`,
-    labelColor: (opacity = 1) => `rgba(0, 0, 0, 1)`,
-    paddingLeft: 0,
-    paddingRight: 0,
-    barPercentage: 0.5,
-  };
+  function previousMonth(): void {
+    setCurrentDetailsMonth(prev => {
+      let m = new Date(prev)
+      m.setMonth(prev.getMonth() - 1);
+      return m;
+    })
+  }
 
-  const graphStyle = {
-    marginVertical: 10,
-    borderRadius: 5,
-    ...chartConfig
-  };
-
-  const chartData = {
-    labels: ["Jan.", "Fév.", "Mar.", "Avr.", "Mai", "Juin", "Juil.", "Août", "Sep.", "Oct.", "Nov.", "Déc."],
-    datasets: [
-      {
-        data: savingData
-      }
-    ]
-  };
-
-  const chartDataPerCategories = {
-    labels: categoriesLabel,
-    datasets: [
-      {
-        data: categoriesData
-      }
-    ]
-  };
 
 
 
   return (
-      <SafeAreaView style={[styles.content]}>
-        <View style={{ flex: 1, justifyContent: "flex-start"}}>
-          <Text style={[styles.white, { fontWeight: "bold", fontSize: 25, textAlign: "center" }]}>{date.GetYearAsString(today)}</Text>
-          <Text style={[styles.white, { fontWeight: "bold", fontSize: 12, textAlign: "center" }]}>Economies: {totalSaving.toFixed()}</Text>
-          <View style={ { alignSelf: "center"}} >
-            
-            <BarChart
-              width={Math.min(Dimensions.get("window").width - 20, 560)}
-              height={220}
-              chartConfig={chartConfig}
-              style={graphStyle}
-              withInnerLines={false}
-              showValuesOnTopOfBars={true}
-              fromZero={true}
-              showBarTops={false}
-              yAxisInterval={0}
-              yAxisLabel=""
-              yAxisSuffix=""
-              data={chartData}
-            />
-          </View>
-          <View>
-            <Text style={[styles.white, { fontWeight: "bold", fontSize: 25, textAlign: "center" }]}> {'<-'} {date.GetMonthAsString(lastMonth)} {'->'}</Text>
-          </View>
-          <View style={ { alignSelf: "center"}} >
-            <BarChart
-              width={250}
-              height={Math.min(Dimensions.get("window").width - 20, 560)}
-              chartConfig={chartConfig}
-              verticalLabelRotation={270}
+    <SafeAreaView style={[styles.content]}>
+      <View style={{ flex: 1, justifyContent: "flex-start" }}>
+        <Text style={[styles.white, { fontWeight: "bold", fontSize: 25, textAlign: "center" }]}>{date.GetYearAsString(today)}</Text>
+        <Text style={[styles.white, { fontWeight: "bold", fontSize: 12, textAlign: "center" }]}>Economies: {totalSaving.toFixed()}</Text>
+        <View style={{ alignSelf: "center" }} >
+
+          <VictoryChart domainPadding={0}
+            width={Math.min(Dimensions.get("window").width, 560)}
+            height={220}
+            padding={{ top: 40, bottom: 60, left: 20, right: 20 }}
+            >
+            <VictoryGroup data={savingData}>
+              <VictoryBar                 
+                labels={({ datum }) => datum.y !== 0 ? datum.y : ''}
+                style={{
+                  labels: { fill: "white" }
+                }}
+              />
+              <VictoryBar
+                labels={({ datum }) => datum.x }
+                style={{
+                  data: { fill: "#29C7AC" }, labels: { fill: "white" }
+                }}
+                labelComponent={<NegativeAwareTickLabel />}
+              />
+            </VictoryGroup>
+
+            <VictoryAxis
               style={{
-                marginTop: -20,
-                transform: [{ rotate: "90deg" }],
-                paddingLeft: 0,
-                paddingRight: 10,
-                borderRadius: 5                
+                tickLabels: { fill: "none" },
+                axis: {
+                  stroke: 'white'
+                },
               }}
-              xLabelsOffset={40}
-              withInnerLines={false}
-              showValuesOnTopOfBars={true}
-              fromZero={true}
-              showBarTops={false}
-              yAxisLabel=""
-              yAxisSuffix=""
-              data={chartDataPerCategories}
             />
-          </View>
+          </VictoryChart>
         </View>
-          
-        <MenuBar navigation={navigation} hasBack={true} showActions={false} showRecurring={false}></MenuBar>
-      </SafeAreaView>
+        <View style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+
+          <Ionicons name="md-arrow-back" size={35} color="#fff" style={{ zIndex: 3, height: 35, marginRight: 20 }} onPress={() => previousMonth()} />
+          <Text style={[styles.white, { fontWeight: "bold", fontSize: 25, textAlign: "center" }]}>{date.GetMonthAsString(currentDetailsMonth)}</Text>
+          {currentDetailsMonth.getMonth() !== lastMonth.getMonth() &&
+            <Ionicons name="md-arrow-forward" size={35} color="#fff" style={{ zIndex: 3, height: 35, marginLeft: 20 }} onPress={() => nextMonth()} />
+          }
+        </View>
+        <View style={{ alignSelf: "center" }} >
+          {  categoriesData.length !== 0
+          && 
+            <VictoryChart horizontal
+            width={Math.min(Dimensions.get("window").width, 560)}
+            height={300}
+            padding={50}>
+            <VictoryStack
+              style={{ data: { width: 15 }, labels: { fontSize: 15, fill: "white" } }}
+            >
+              <VictoryBar
+                style={{ data: { fill: "#29C7AC" }, }}
+                data={categoriesData}
+
+                labels={({ datum }) => datum.y }
+              />
+            </VictoryStack>
+
+            <VictoryAxis
+              style={{
+                axis: { stroke: "transparent" },
+                ticks: { stroke: "transparent" },
+                tickLabels: { fontSize: 14, fill: "white" }
+              }}
+              tickLabelComponent={
+                <VictoryLabel
+                  x={0}
+                  textAnchor="start"
+                />
+              }
+            />
+          </VictoryChart>
+          }
+        </View>
+      </View>
+
+      <MenuBar navigation={navigation} hasBack={true} showActions={false} showRecurring={false}></MenuBar>
+    </SafeAreaView>
   );
 }
